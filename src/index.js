19a7c2b55d59d34a07d9130fbaecb003a7684169
MINSK\Siarhei_Huk <eventpromoby@gmail.com>
2018-02-21 14:14:17 +0300

    Implementation

--- src/index.js
+++ src/index.js
@@ -1,30 +1,70 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.data = [];
+    this.comparator = function(a, b){
+      return a - b;
+    };
   }
 
   add(element) {
-    // your implementation
+    this.data.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.data[index];
   }
 
   get length() {
-    // your implementation
+    return this.data.length;
   }
 
   toArray() {
-    // your implementation
+    return this.data;
   }
 
   sort(indices) {
-    // your implementation
+    indices = this.clearIndices(indices);
+    var temp = this.sortableData(indices);
+    var isPart = temp != this.data;
+    temp = temp.sort(this.comparator);
+    if(isPart){
+      this.updatePart(indices, temp);
+    }else{
+      this.data = temp;
+    }
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.comparator = compareFunction;    
+  }
+
+  clearIndices(indices){
+    if(!indices){
+      return indices;
+    }
+    var data = this.data;
+    return indices.filter(i => {
+      return this.data[i] !== void 0;
+    }).sort();
+  }
+
+  sortableData(indices){
+    if(!indices || indices.length == 0){
+      return this.data;
+    }
+    var part = [];  
+    indices.forEach(i => {
+      part.push(this.data[i]);
+    });    
+    return part;
+  }
+
+  updatePart(indices, part){
+    var index = 0;
+    indices.forEach(i => {
+      this.data[i] = part[index++];      
+    }); 
+    this.data
   }
 }
 
